039d7f88202c3afd4f597268d025c7f4461fb7ea
nibral <nibral@dancingstarson.me>
2016-04-04 00:12:46 +0900

    Implement recording AGQR by rtmpdump

--- test/test.js
+++ test/test.js
@@ -1,6 +1,26 @@
 'use strict';
 
 /*
+    ./lib/agqr.js
+*/
+const agqr = require('../lib/agqr');
+agqr.getStreamUrl().then((streamUrl) => {
+    console.log('agqr.getstreamUrl()');
+    console.log(streamUrl);
+});
+
+/*
+    ./lib/recorder.js
+*/
+const recorder = require('../lib/recorder');
+agqr.getStreamUrl().then((streamUrl) => {
+    return recorder.record(streamUrl, 10);
+}).then((flvFilePath) => {
+    console.log('recorder.record()');
+    console.log(flvFilePath);
+});
+
+/*
     scheduler
 */
 const scheduler = require('../lib/scheduler');
