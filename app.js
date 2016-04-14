b6f915966e40720b572743797453467fb010e65c
nibral <nibral@dancingstarson.me>
2016-04-14 12:11:11 +0900

    Implement basic authentication

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+// authentication
+const basicAuth = require('basic-auth-connect');
+app.use(basicAuth('udon', 'kamatama'));
+
 // routing
 const index = require('./routes');
 app.use('/', index);
